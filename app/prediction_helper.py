85642adcd9b187ddbf4761ca29a0ea9bfbaa8ac4
Rushabh <rushabh@Rushabhs-MacBook-Air.local>
2025-10-21 12:25:59 -0700

    push streamlit app code

---
+++ app/prediction_helper.py
@@ -0,0 +1,127 @@
+import pandas as pd
+from joblib import load
+
+model_rest = load("/Users/rushabh/Documents/DS-ML/Healthcare-Premium-Analysis/app/artifacts/model_rest.joblib")
+model_young = load("/Users/rushabh/Documents/DS-ML/Healthcare-Premium-Analysis/app/artifacts/model_young.joblib")
+
+scaler_rest = load("/Users/rushabh/Documents/DS-ML/Healthcare-Premium-Analysis/app/artifacts/scaler_rest.joblib")
+scaler_young = load("/Users/rushabh/Documents/DS-ML/Healthcare-Premium-Analysis/app/artifacts/scaler_young.joblib")
+
+def calculate_normalized_risk(medical_history):
+    risk_scores = {
+        "diabetes": 6,
+        "heart disease": 8,
+        "high blood pressure": 6,
+        "thyroid": 5,
+        "no disease": 0,
+        "none": 0
+    }
+    diseases = medical_history.lower().split(" & ")
+
+    total_risk_score = sum(risk_scores.get(disease, 0) for disease in diseases)  # Default to 0 if disease not found
+
+    max_score = 14 # risk score for heart disease (8) + second max risk score (6) for diabetes or high blood pressure
+    min_score = 0  # Since the minimum score is always 0
+
+    # Normalize the total risk score
+    normalized_risk_score = (total_risk_score - min_score) / (max_score - min_score)
+
+    return normalized_risk_score
+
+def preprocess_input(input_dict):
+    # Define the expected columns and initialize the DataFrame with zeros
+    expected_columns = [
+        'age', 'number_of_dependants', 'income_lakhs', 'insurance_plan', 'genetical_risk', 'normalized_risk_score',
+        'gender_Male', 'region_Northwest', 'region_Southeast', 'region_Southwest', 'marital_status_Unmarried',
+        'bmi_category_Obesity', 'bmi_category_Overweight', 'bmi_category_Underweight', 
+        'smoking_status_Occasional', 'smoking_status_Regular', 'smoking_status_No Smoking',   # ✅ Added missing column
+        'employment_status_Salaried', 'employment_status_Self-Employed'
+    ]
+
+    insurance_plan_encoding = {'Bronze': 1, 'Silver': 2, 'Gold': 3}
+
+    df = pd.DataFrame(0, columns=expected_columns, index=[0])
+
+    # Manually assign values for each categorical input based on input_dict
+    for key, value in input_dict.items():
+        if key == 'Gender' and value == 'Male':
+            df['gender_Male'] = 1
+        elif key == 'Region':
+            if value == 'Northwest':
+                df['region_Northwest'] = 1
+            elif value == 'Southeast':
+                df['region_Southeast'] = 1
+            elif value == 'Southwest':
+                df['region_Southwest'] = 1
+        elif key == 'Marital Status' and value == 'Unmarried':
+            df['marital_status_Unmarried'] = 1
+        elif key == 'BMI Category':
+            if value == 'Obesity':
+                df['bmi_category_Obesity'] = 1
+            elif value == 'Overweight':
+                df['bmi_category_Overweight'] = 1
+            elif value == 'Underweight':
+                df['bmi_category_Underweight'] = 1
+        elif key == 'Smoking Status':
+            if value == 'Occasional':
+                df['smoking_status_Occasional'] = 1
+            elif value == 'Regular':
+                df['smoking_status_Regular'] = 1
+            elif value == 'No Smoking':              
+                df['smoking_status_No Smoking'] = 1  
+        elif key == 'Employment Status':
+            if value == 'Salaried':
+                df['employment_status_Salaried'] = 1
+            elif value == 'Self-Employed':
+                df['employment_status_Self-Employed'] = 1
+        elif key == 'Insurance Plan':
+            df['insurance_plan'] = insurance_plan_encoding.get(value, 1)
+        elif key == 'Age':
+            df['age'] = value
+        elif key == 'Number of Dependants':
+            df['number_of_dependants'] = value
+        elif key == 'Income in Lakhs':
+            df['income_lakhs'] = value
+        elif key == "Genetical Risk":
+            df['genetical_risk'] = value
+
+    df['normalized_risk_score'] = calculate_normalized_risk(input_dict['Medical History'])
+    df = handle_scaling(input_dict['Age'], df)
+
+    return df
+
+def handle_scaling(age, df):
+    # scale age and income_lakhs column
+    if age <= 25:
+        scaler_object = scaler_young
+    else:
+        scaler_object = scaler_rest
+
+    cols_to_scale = scaler_object['cols_to_scale']
+    scaler = scaler_object['scaler']
+
+    df['income_level'] = None # since scaler object expects income_level supply it. This will have no impact on anything
+    df[cols_to_scale] = scaler.transform(df[cols_to_scale])
+
+    df.drop('income_level', axis='columns', inplace=True)
+
+    return df
+
+def predict(input_dict):
+    input_df = preprocess_input(input_dict)
+
+    if input_dict['Age'] <= 25:
+        model = model_young
+    else:
+        model = model_rest
+
+    trained_feature_names = model.feature_names_in_
+
+    for col in trained_feature_names:
+        if col not in input_df.columns:
+            input_df[col] = 0
+
+    input_df = input_df[trained_feature_names]
+
+    prediction = model.predict(input_df)
+    return int(prediction[0])
